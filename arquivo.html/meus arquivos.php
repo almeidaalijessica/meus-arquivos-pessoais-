a08782501849e3d5647b5596a01b6e69bdd7d701
jessica almeida de oliveira <jessicajflalmeida@gmail.com>
2020-10-06 14:37:59 -0300

    html

---
+++ arquivo.html/meus arquivos.php
@@ -0,0 +1,55 @@
+<HTML>
+<HEAD>
+ <TITLE>Tudo sobre flores</TITLE>
+</HEAD>
+<BODY>
+    <h1>Tipos de flores e suas funções</h1>
+    <p>A flor é a estrutura responsável pela reprodução das plantas angiospermas.
+
+É por meio da reprodução que novas plantas são originadas, assegurando a manutenção dos ecossistemas.</p>
+
+<h2>Funções das flores </h2>
+A função primordial das flores é a produção de sementes
+para a formação de novas plantas, garantindo a sobrevivência das espécies.
+
+Assim, a flor é responsável pela reprodução das plantas.
+Para isso, elas são formadas por folhas modificadas, geralmente de cores atrativas e formatos diferenciados para atrair os seus polinizadores <br>
+<h3>Partes das flores</h3>
+<img src="C:/Users/familia almeida/Desktop/partesdaflor-cke.jpg" width="630" height="358" border="0"> <br>
+Uma flor completa apresenta as seguintes estruturas:
+
+Estame: estrutura masculina da flor onde localizam-se o filete e a antera.
+Carpelo: estrutura feminina da flor, formada pelo estigma, estilete e ovário.
+Pétalas: folhas modificadas e coloridas com a função de atrair os polinizadores. O conjunto de pétalas é chamado de corola.
+Sépalas: localizadas abaixo das pétalas, geralmente, de coloração verde. O conjunto de sépalas é chamado de cálice.
+Toda essa estrutura é sustentada pelo pedúnculo, haste responsável por ligar a flor à planta.
+
+O pedúnculo apresenta uma porção dilatada ligada à flor denominada de receptáculo floral, onde estão inseridos os elementos florais.
+<h4>Gineceu e Androceu</h4>
+<img src="../florfemininaemasculina-cke.jpg" width="630" height="405" border="0">
+De acordo com a estrutura da flor, ela pode ser feminina ou masculina. Essa definição depende da presença do gineceu e do androceu.
+
+<h3>Gineceu  </h3>
+O conjunto de carpelos é denominado de gineceu, a parte feminina da flor.
+
+O gineceu é formado por carpelos, pistilo, estigma, estilete e ovário. Dentro do ovário estão os gametas femininos da planta.
+
+O estigma é a porção que recebe o grão de pólen e através do estilete liga-se ao ovário. Já o ovário é a parte que vai se transformar em fruto.
+
+O fruto é resultado do desenvolvimento do ovário, enquanto a semente representa o desenvolvimento do óvulo depois da fecundação.
+
+<h4>Androceu</h4>
+O conjunto de estames é denominado de androceu, a parte masculina da flor. O androceu é formado pelos estames, antera e filete.
+
+Os estames são formados pela antera e filete. O filete corresponde a uma haste longa e fina, onde em sua extremidade encontra-se a antera, responsável pela produção do pólen. <br>
+
+
+Leia também:
+
+Partes da Planta
+Reino Vegetal
+Botânica: o estudo das plantas
+Tipos de flores
+
+</BODY>
+</HTML>
