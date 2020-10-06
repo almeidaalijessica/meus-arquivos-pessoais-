23874f8a46a4037567ea9c97419d9a0e4e890f3c
jessica almeida de oliveira <jessicajflalmeida@gmail.com>
2020-10-06 15:03:03 -0300

    mais um aquivos.py

---
+++ aula08.py
@@ -0,0 +1,4 @@
+import math
+num=int(input("digite um numero"))
+raiz=math.sqrt(num)
+print("a raiz {:.2f} e igual a {}".format(num,raiz))
---
+++ aula09.py
@@ -0,0 +1,3 @@
+frase="curso em video pythom"
+dividido=frase.split()
+print(dividido[3])
